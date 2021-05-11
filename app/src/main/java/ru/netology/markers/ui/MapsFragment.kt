d7efc6d484a44a8c72d8c9f07e49e2e811f152c1
Dmitriy Dolgov <dolgov2007@inbox.ru>
2021-05-11 15:19:28 +0300

    fix map demo

--- app/src/main/java/ru/netology/markers/ui/MapsFragment.kt
+++ app/src/main/java/ru/netology/markers/ui/MapsFragment.kt
@@ -1,23 +1,36 @@
 package ru.netology.markers.ui
 
+import android.R
+import android.graphics.Color
+import android.graphics.PointF
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.coroutineScope
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.layers.ObjectEvent
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.CompositeIcon
+import com.yandex.mapkit.map.IconStyle
+import com.yandex.mapkit.map.RotationType
 import com.yandex.mapkit.mapview.MapView
-import ru.netology.markers.R
+import com.yandex.mapkit.user_location.UserLocationLayer
+import com.yandex.mapkit.user_location.UserLocationObjectListener
+import com.yandex.mapkit.user_location.UserLocationView
+import com.yandex.runtime.image.ImageProvider
+import ru.netology.markers.databinding.FragmentMapsBinding
 
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), UserLocationObjectListener {
     private lateinit var mapView: MapView
+    private lateinit var userLocationLayer: UserLocationLayer
+
     private val TARGET_LOCATION: Point = Point(59.945933, 30.320045)
     private val MAPKIT_API_KEY = "3fcb8c8a-5fec-46d6-9d5c-446473cfa6ab"
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,24 +38,85 @@ class MapsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         MapKitFactory.setApiKey(MAPKIT_API_KEY)
-        MapKitFactory.initialize(context)
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        MapKitFactory.initialize(this.context)
+
+        val binding = FragmentMapsBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        mapView = binding.mapview
+        mapView.map.move(
+            CameraPosition(TARGET_LOCATION, 14.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 5F),
+            null
+        )
+        mapView.map.apply {
+            isLiteModeEnabled
+            isValid
+            isRotateGesturesEnabled = false
+        }
+
+        val mapKit = MapKitFactory.getInstance()
+        val userLocationLayer = mapKit.createUserLocationLayer(mapView.mapWindow)
+        userLocationLayer.isVisible = true
+        userLocationLayer.isHeadingEnabled = true
+
+        userLocationLayer.setObjectListener(this)
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
 
-//        mapView = mapView.findViewById(R.id.mapview)
-//        mapView.map.move(
-//            CameraPosition(TARGET_LOCATION, 14.0f, 0.0f, 0.0f),
-//            Animation(Animation.Type.SMOOTH, 5F),
-//            null
-//        )
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapView.onStart()
+    }
 
-        val mapFragment = childFragmentManager.findFragmentById(R.id.mapview) as MapsFragment
+    override fun onObjectAdded(userLocationView: UserLocationView) {
+        userLocationLayer.setAnchor(
+            PointF((mapView.width * 0.5).toFloat(), (mapView.height * 0.5).toFloat()),
+            PointF((mapView.width * 0.5).toFloat(), (mapView.height * 0.83).toFloat())
+        )
 
-        lifecycle.coroutineScope.launchWhenCreated {
+        userLocationView.arrow.setIcon(
+            ImageProvider.fromResource(
+                this.context, R.drawable.arrow_up_float
+            )
+        )
 
-        }
+        val pinIcon: CompositeIcon = userLocationView.getPin().useCompositeIcon()
+
+        pinIcon.setIcon(
+            "icon",
+            ImageProvider.fromResource(this.context, R.drawable.sym_def_app_icon),
+            IconStyle().setAnchor(PointF(0f, 0f))
+                .setRotationType(RotationType.ROTATE)
+                .setZIndex(0f)
+                .setScale(1f)
+        )
+
+        pinIcon.setIcon(
+            "pin",
+            ImageProvider.fromResource(this.context, R.drawable.ic_menu_search),
+            IconStyle().setAnchor(PointF(0.5f, 0.5f))
+                .setRotationType(RotationType.ROTATE)
+                .setZIndex(1f)
+                .setScale(0.5f)
+        )
+
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE and -0x66000001)
+    }
+
+    override fun onObjectRemoved(view: UserLocationView) {
+    }
+
+    override fun onObjectUpdated(view: UserLocationView, event: ObjectEvent) {
     }
 }
